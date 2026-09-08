55ccd1d00c8d0cb6d0fd19e3a8b5138c577275e3
Aina-Kevin610 <randrianambinintsoaainakevin@gmail.com>
2026-09-08 17:40:45 +0300

    documentation: window and even

--- Makefile
+++
@@ -1,8 +0,0 @@
-NAME = number_checker
-FLAG = -Wall -Werror -Wextra $(sld2-config --cflags --libs)
-SRCS = main.c
-
-all: $(NAME)
-
-$(NAME): $(OBJS)
-	cc $(FLAG) 
--- main.c
+++ main.c
@@ -1,30 +1,39 @@
-#include <SDL.h>
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_events.h>
 #include <stdio.h>
+#include <unistd.h>
 
-int main()
+int main(void)
 {
-    if (SDL_Init(SDL_INIT_VIDEO) < 0) {
-        printf("Erreur d'initialisation : %s\n", SDL_GetError());
-        return 1;
-    }
+    SDL_Init(SDL_INIT_VIDEO);
 
-    SDL_Window* window = SDL_CreateWindow("Mon Projet SDL", 
-                        SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 
-                        800, 600, SDL_WINDOW_SHOWN);
-
+    SDL_Window *window = SDL_CreateWindow(
+        "Mon jeu SDL2",
+        SDL_WINDOWPOS_CENTERED,
+        SDL_WINDOWPOS_CENTERED,
+        720,
+        480,
+        SDL_WINDOW_SHOWN
+    );
+    SDL_Event   event;
     int running = 1;
-    SDL_Event event;
 
-    // Boucle principale
-    while (running) {
-        while (SDL_PollEvent(&event)) {
-            if (event.type == SDL_QUIT) {
+    while (running)
+    {
+        while (SDL_PollEvent(&event))
+        {
+            if (event.type == SDL_QUIT)
                 running = 0;
+            if (event.type == SDL_KEYDOWN)
+            {
+                printf("keyboard pressed!\n");
+                fflush(NULL);
             }
         }
     }
 
     SDL_DestroyWindow(window);
     SDL_Quit();
+
     return 0;
 }
